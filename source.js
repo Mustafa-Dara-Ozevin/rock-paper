06d805ece77f4b9f55bd12d53eb6d7fc4a486ae9
Mustafa-Dara-Ozevin <mustafadara.ozevin@outlook.com>
2023-04-21 16:47:11 +0200

    Implement the game loop

--- source.js
+++ source.js
@@ -1,24 +1,24 @@
 const ROCK = "rock";
 const PAPER = "paper";
-const SCISSORRS = "scissors";
+const SCISSORS = "scissors";
 
-const choices = [ROCK, PAPER, SCISSORRS];
+const choices = [ROCK, PAPER, SCISSORS];
 
 
 
 function getComputerChoice()
 {
     return choices[Math.floor(Math.random()*choices.length)];
-};
+}
 function getPlayerChoice() {
     return prompt("Rock Paper Scissors?: ").toLowerCase();
 
 }
 
-function play(playerChoice, computerChoice){
+function playRound(playerChoice, computerChoice){
     if (playerChoice === ROCK){
-        if (computerChoice === SCISSORRS){
-            return ("You win! " + ROCK + " beats " + SCISSORRS);
+        if (computerChoice === SCISSORS){
+            return ("You win! " + ROCK + " beats " + SCISSORS);
         }else if(computerChoice === PAPER){
             return ("You lose! " + PAPER + " beats " + ROCK);
         }else{
@@ -27,20 +27,38 @@ function play(playerChoice, computerChoice){
     }else if(playerChoice===PAPER){
         if (computerChoice === ROCK){
             return ("You win! " + PAPER + " beats " + ROCK);
-        }else if(computerChoice === SCISSORRS){
-            return ("You lose! " + SCISSORRS + " beats " + PAPER);
+        }else if(computerChoice === SCISSORS){
+            return ("You lose! " + SCISSORS + " beats " + PAPER);
         }else{
             return "Draw!"
         }
     }else{
         if (computerChoice === PAPER){
-            return ("You win! " + SCISSORRS + " beats " + PAPER);
+            return ("You win! " + SCISSORS + " beats " + PAPER);
         }else if(computerChoice === ROCK){
-            return ("You lose! " + ROCK + " beats " + SCISSORRS);
+            return ("You lose! " + ROCK + " beats " + SCISSORS);
         }else{
             return "Draw!"
         }
     }
+
 }
 
-console.log(play(getPlayerChoice(),getComputerChoice()));
+function game(){
+    let playerPoint = 0;
+    let computerPoint = 0;
+    for(let i=0;i<5;i++){
+        let result = playRound(getPlayerChoice(),getComputerChoice());
+        console.log(result)
+        if(result.includes("win")){
+            playerPoint++;
+        }else if (result.includes("lose")){
+            computerPoint++;
+        }
+    }
+    console.log("Your Score: " + playerPoint)
+    console.log("Computer Score: " + computerPoint)
+
+}
+
+game()
